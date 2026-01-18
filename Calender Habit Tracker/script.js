7a7ab608fef917a182d1afd4c73a868da93d45dc
phiona-dev <phionadev@gmail.com>
2026-01-18 21:29:21 +0300

    starter structure

--- Calender Habit Tracker/script.js
+++ Calender Habit Tracker/script.js
@@ -1,1 +1,17 @@
 //this is a calendar-based habit and streak system
+const startbtn = document.getElementById("start-btn")
+const habitPage = document.getElementById("habit-form-screen")
+const welcomePage = document.getElementById("welcome-screen")
+const calendarPage = document.getElementById("calendar-screen")
+const nextbtn = document.getElementById("habit-form-btn")
+
+startbtn.addEventListener("click", () => {
+    console.log("startbtn clicked")
+    habitPage.classList.remove("hidden") //force show
+    welcomePage.classList.add("hidden") //force hide
+})
+
+nextbtn.addEventListener("click", () => {
+    habitPage.classList.add("hidden")
+    calendarPage.classList.remove("hidden")
+})
