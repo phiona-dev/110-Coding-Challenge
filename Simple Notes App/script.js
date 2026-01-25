bfebc2fea64f74d80d18750023469bffd284a18d
phiona-dev <phionadev@gmail.com>
2026-01-25 11:40:04 +0300

    simple notes app - adding and displaying notes

---
+++ Simple Notes App/script.js
@@ -0,0 +1,42 @@
+const notesForm = document.getElementById("notes-form")
+const btn = document.getElementById("btn")
+
+let notesArray = JSON.parse(localStorage.getItem("notesArray")) || []
+
+notesForm.addEventListener("submit", (e) => {
+    e.preventDefault()
+
+    const title = document.getElementById("notes-title").value
+    const notes = document.getElementById("notes").value
+
+    const newNotes = {
+        id: Date.now(),
+        title: title,
+        content: notes,
+        createdAt: Date.now()
+    }
+
+    notesArray.push(newNotes)
+    saveNotes()
+    renderNotes()
+    notesForm.reset()
+    console.log(notesArray)
+})
+ function saveNotes() {
+    localStorage.setItem("notesArray", JSON.stringify(notesArray))
+ }
+
+function renderNotes(){
+    const notesBody = document.getElementById("notes-body")
+    notesBody.innerHTML = ""
+    notesArray.forEach(item => {
+        const li = document.createElement("li")
+        const contentDiv = document.createElement("div")
+        li.textContent = item.title
+        contentDiv.textContent = item.content
+
+        notesBody.appendChild(li)
+        notesBody.appendChild(contentDiv)
+    })
+}
+renderNotes()
